e85ab0c5eb4764b59fd3b32660d8db3ecb2dce00
Roman <rograf@edu.aau.at>
2020-03-15 16:08:34 +0100

    initialised items and updated text

--- app/src/main/java/com/example/seii/MainActivity.java
+++ app/src/main/java/com/example/seii/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.seii;
 
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button;
+    EditText input;
+    TextView textTop;
+    TextView textBottom;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button)findViewById(R.id.button);
+        button.setText("Los!");
+
+        input = (EditText)findViewById(R.id.editText);
+
+        textTop = (TextView)findViewById(R.id.textView);
+        textTop.setText("Matrikelnummer");
+
+        textBottom = (TextView)findViewById(R.id.textView2);
+        textBottom.setText("...");
+
+
+
+
     }
 }
